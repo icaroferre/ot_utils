90be742d38f22dfed36d05e02752b98b9a405a6b
Icaro Ferre <icaroferre+bitbucket@gmail.com>
2020-07-20 15:07:47 -0300

    Updated version number

---
+++ target/rls/debug/deps/ot_utils-fd0afaa4b397ee55.d
@@ -0,0 +1,5 @@
+/Users/icaroferre/Dropbox/Work/GitHub/ot_utils/target/rls/debug/deps/ot_utils-fd0afaa4b397ee55.rmeta: src/lib.rs
+
+/Users/icaroferre/Dropbox/Work/GitHub/ot_utils/target/rls/debug/deps/ot_utils-fd0afaa4b397ee55.d: src/lib.rs
+
+src/lib.rs:
